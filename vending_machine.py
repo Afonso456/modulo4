5c2b84cea00f7804c433be17028776bf326086b2
Afonso <ac00090@gmail.com>
2025-02-04 08:48:27 +0000

    04/02

--- vending_machine.py
+++ vending_machine.py
@@ -8,21 +8,27 @@ moedas= np.array([0.01,0.02,0.05,0.10,0.20,0.50,1,2])
 stock= np.array([2,2,2,2,2,2,2,2])
 
 #preço do produto
-preco= float(input("Introduza o preço do produto:"))
+valor_pagar= float(input("Introduza o preço do produto:"))
 #valor entregue pelo cliente
 valor_entregue= float(input("Valor entregue:"))
 #calcular o troco
-troco= valor_entregue - preco
+troco= valor_entregue - valor_pagar
 if troco == 0:
     print("Volte sempre")
 else:
 #procurar as quantias que prefaazem a quantia do troco
     while troco > 0:
+        encontra = False
         for m in range(len(moedas)-1,-1,-1):
             moedas_devolver= ""
-            if moedas[m] < troco and stock[m] > 0:
+            if moedas[m] <= troco and stock[m] > 0:
                 moedas_devolver= moedas_devolver + str(moedas[m]) + ","
+                troco= troco - round(moedas[m],2)
+                troco= round(troco,2)
                 stock[m] = stock[m] - 1
+                encontra = True
+                break
+            if encontra == False:
                 break
     print(f"Moedas a devolver {moedas_devolver} e o troco restante é de {troco}")
     print(moedas,stock)
